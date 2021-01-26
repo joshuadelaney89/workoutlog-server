4ffc9581dc030a13e2db531814814e400739c8a9
joshuadelaney89 <73564714+joshuadelaney89@users.noreply.github.com>
2021-01-26 00:44:19 -0500

    second push

--- controllers/logcontroller.js
+++ controllers/logcontroller.js
@@ -26,16 +26,6 @@ router.post('/create', validateSession, (req, res) => {
 })
 
 /******************************
-    *** GET ALL ENTRIES ***
-******************************/
-
-router.get("/", (req, res) => {
-    Log.findAll()
-    .then(logs => res.status(200).json(logs))
-    .catch(err => res.status(500).json({error: err}))
-});
-
-/******************************
   *** GET ENTRIES BY USER ***
 ******************************/
 
